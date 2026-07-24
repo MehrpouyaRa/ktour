93c455d6d736849744ef67e7d462df45db06eb3d
MehrpouyaRa <mehrpouya.ramzi@gmail.com>
2026-07-25 02:18:48 +0330

    log

--- src/component/pages/landing/LandingPage.tsx
+++ src/component/pages/landing/LandingPage.tsx
@@ -5,8 +5,6 @@ import LandingMessage from './parts/LandingMessage'
 import Footer from '@/component/layout/Footer'
 
 function LandingPage() {
-  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log(process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
   
   return (
     <div className='flex flex-col gap-6 md:gap-10'>
